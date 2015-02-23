4ad794128f6c03ac81b606d07c770aa64be9a92b
Dan Wright & Christopher Joy-Webb <student@epicodus.com>
2015-02-23 10:37:39 -0800

    add basic functionality

---
+++ js/StudentsController.js
@@ -0,0 +1,11 @@
+studentRoster.controller("StudentCtrl", function StudentCtrl($scope) {
+  $scope.students = [];
+  $scope.addStudent = function() {
+    $scope.students.push({ name: $scope.studentName });
+    $scope.studentName = null;
+  };
+  $scope.deleteStudent = function(student) {
+    var index = $scope.students.indexOf(student)
+    $scope.students.splice(index, 1);
+  };
+});
